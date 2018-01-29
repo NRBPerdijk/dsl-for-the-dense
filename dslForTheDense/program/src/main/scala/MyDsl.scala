d5b13403770d5b73379bc03e06d1cd2b555ab20a
NRBPerdijk <nathanperdijk@zoho.com>
2018-01-29 13:03:11 +0100

    MyDsl starter

---
+++ dslForTheDense/program/src/main/scala/MyDsl.scala
@@ -0,0 +1,50 @@
+
+
+package object MyDsl extends Implicits {
+
+}
+
+trait Implicits {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+//import ExternalApi.{StartingShore => ExternalApiStartingShore}
+//import ExternalApi.{DestinationShore => ExternalApiDestinationShore}
+//import ExternalApi.{Boat => ExternalApiBoat}
+//import ExternalApi.{Creature => ExternalApiCreature}
+//import ExternalApi.{Cabbage => ExternalApiCabbage}
+//import ExternalApi.{Sheep => ExternalApiSheep}
+//import ExternalApi.{Wolf => ExternalApiWolf}
+//import ExternalApi.{Shore => ExternalApiShore}
